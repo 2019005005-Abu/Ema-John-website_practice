14341c34f29feefda10a1efca2b400e6a84ac36d
Abu Al Shahriar Rifat <rifatrifatsharda2686@gmail.com>
2023-03-28 17:21:00 +0600

    Cart stored in local-Storaged

--- Component/Shop.jsx
+++ Component/Shop.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { addToDb, getShoppingCart } from '../assets/utilities/fakedb';
 import "../Component/Shop.css"
 import Product from '../Product/Product';
 import Cart from './Cart/Cart';
@@ -16,10 +17,21 @@ const Shop = () => {
      })
     }
  
+    React.useEffect(()=>{
+       storedDataInformation()
+    },[])
+
+    const storedDataInformation=()=>{
+       const storedCart=getShoppingCart();
+       console.log(storedCart)
+    }
+
+
     const handledToCart=(product)=>{
       console.log('Product has added',product)
       const newCart=[...cart,product];
       setCart(newCart);
+      addToDb(product.id)
     }
   return (
     <div className='shop-container'>
